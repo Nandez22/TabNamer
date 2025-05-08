d016c15d2707a067c6daebbcb8e74c71d3a2c57d
Joe Hernandez <112195044+Nandez22@users.noreply.github.com>
2025-05-08 03:03:27 -0500

    Added Loading on tab refresh and switch

--- Scripts/Background.js
+++ Scripts/Background.js
@@ -2,15 +2,19 @@ import { Rename, TryLoadRename } from "./Functions.js";
 
 chrome.tabs.onActivated.addListener(async (activeInfo) => {
     const tab = await chrome.tabs.get(activeInfo.tabId);
-    if(tab?.url?.startsWith("chrome://")) return;
 
+    console.log("Switch:", activeInfo.title);
+
+    if(tab?.url?.startsWith("chrome://")) return;
     if(tab) Rename(tab, await TryLoadRename(tab.url));
 });
 
-//We don't actually need to track new tabs since they appear either as a switch or change
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if(changeInfo.title) {
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    if(changeInfo) {
         console.log("Change:", changeInfo.title);
+
+        if(tab?.url?.startsWith("chrome://")) return;
+        if(tab) Rename(tab, await TryLoadRename(tab.url));
     }
 });
